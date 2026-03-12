8da17abb2762c1ddf56ae72aa6222a1147cfeb44
Joe Crowley <joseph.crowley@synergy.net.au>
2026-03-12 09:20:57 +0800

    fix(admin): hookspecs and impls updated for setup and teardown to include step

--- src/cactus_client/admin/default_impl.py
+++ src/cactus_client/admin/default_impl.py
@@ -8,12 +8,12 @@ from typing import Any
 
 class DefaultPlugin:
     @hookimpl(trylast=True)
-    async def admin_setup(self, context: ExecutionContext) -> ActionResult:
+    async def admin_setup(self, step: StepExecution, context: ExecutionContext) -> ActionResult:
         """General setup function to be run at the beginning of all test runs."""
         return ActionResult.done()
 
     @hookimpl(trylast=True)
-    async def admin_teardown(self, context: ExecutionContext) -> ActionResult:
+    async def admin_teardown(self, step: StepExecution, context: ExecutionContext) -> ActionResult:
         """General teardown function to be run at the end of all test runs."""
         return ActionResult.done()
 
